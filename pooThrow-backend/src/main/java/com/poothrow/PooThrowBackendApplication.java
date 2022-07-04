97a197ac7f7fd14ff213bd55c1dc27309591b8fe
harrison <dlrwjd88@naver.com>
2022-07-04 16:55:53 +0900

    backend basic setting

---
+++ pooThrow-backend/src/main/java/com/poothrow/PooThrowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.poothrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooThrowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooThrowBackendApplication.class, args);
+	}
+
+}
