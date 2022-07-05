f27200a163022338f5bdce3cee5aef3532946c5d
harrison <dlrwjd88@naver.com>
2022-07-05 13:07:22 +0900

    spring - react basic setting
    proxy : port 8080

---
+++ pooThrow/src/main/java/com/poothrow/TestController.java
@@ -0,0 +1,16 @@
+package com.poothrow;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("Test")
+	public List<String> Test(){
+		return Arrays.asList("테스트", "Test");
+	}
+}
