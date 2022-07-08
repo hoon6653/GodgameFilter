a28ef3dca1f2ff3e4e1c1178e6313cb1bc769425
주익정 <dlrwjd88@naver.com>
2022-07-08 17:54:09 +0900

    spring-react basic setting

---
+++ pooThrow/src/main/java/com/cozi/poothrow/PooThrowApplication.java
@@ -0,0 +1,13 @@
+package com.cozi.poothrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooThrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PooThrowApplication.class, args);
+    }
+
+}
