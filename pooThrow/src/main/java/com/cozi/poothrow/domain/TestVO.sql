c2f20d6859da92926e2129d81b322b52fc9e4e95
주익정 <dlrwjd88@naver.com>
2022-07-14 14:58:31 +0900

    react-router-dom 2

---
+++ pooThrow/src/main/java/com/cozi/poothrow/domain/TestVO.sql
@@ -0,0 +1,12 @@
+create database POOTHROW_DB;
+
+use POOTHROW_DB;
+
+create table TEST(ID int, NAME char(20), EMAIl char(30));
+
+insert into Test values (1, "주익정", "fucker001@nate.com");
+insert into Test values (2, "조희찬", "hee@naver.com");
+insert into Test values (3, "박재연", "park@gmail.com");
+insert into Test values (4, "백지훈", "jihun@daum.net");
+
+select * from TEST;
